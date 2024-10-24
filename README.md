65c6351ceab2192a2227a72c96f7fdef73988a17
z6601821 <z6601821@gmail.com>
2024-10-24 13:55:53 -0400

    Major script changes
    Modified script to output each batch of 1000 pulled messages to file
    and keep appending instead of attempting to pull all messages first before
    finally writing all messages at once to the final file.
    Modified script to accept a start message ID so user can opt to start
    pulling messages from a specific point and not need to re-pull channel's
    entire message history.
    Modified the script to be run in another .py file instead of running from
    command line in bash.

--- README.md
+++ README.md
@@ -1,35 +1,66 @@
 # TelegramChannelScraper
-This is a Python command line tool that pulls all intact (non-deleted, non-hidden) messages for a target channel using Telethon to interact with the Telegram API.
+This is a Python script that pulls all intact (non-deleted, non-hidden) messages for a target channel using Telethon to interact with the Telegram API.
 
 ## Requirements
 - Python 3.11.X
 - Git Bash
+- Visual Studio Code (or similar text editor)
 - Telegram API acount and credentials:
-   - api_id
-   - api_hash
-   - account phone number
+   - API ID
+   - API hash
+   - Phone number associated with the account
 
 ## Installation
+[In Git Bash]
+
 Clone with HTTPS:
 
-```bash
-git clone https://github.com/z6601821/TelegramChannelScraper.git
+```
+$ git clone https://github.com/z6601821/TelegramChannelScraper.git
 ```
 
 Change directory into the local repository:
 
-```bash
-cd TelegramChannelScraper/
+```
+$ cd TelegramChannelScraper/
 ```
 
 Install dependencies:
 
-```bash
-pip install -r requirements.txt
+```
+$ pip install -r requirements.txt
 ```
 
 ## Usage
+[In Visual Studio Code or text editor]
 
-```bash
-python main.py
+Create and run the scraper in a separate .py file:
+```
+# Imports
+from TelegramChannelScraper import TelegramChannelScraper
+import asyncio
+
+# Create a scraper object and pass in API credentials
+scraper = TelegramChannelScraper(
+    credentials={
+        "api_id": "<your API ID>", 
+        "api_hash": "<your API hash>", 
+        "phone": "<your account phone number>",
+        }
+)
+
+# Option 1: provide a channel and pull all messages, OR
+asyncio.run(
+    scraper.get_messages(
+        channel_name="<channel name>",
+    )
+)
+
+# Option 2: provide a channel and pull from a specific message ID onwards
+asyncio.run(
+    scraper.get_messages(
+        channel_name="<channel name>",
+        start_message_id=<message ID>,
+    )
+)
 ```
--- TelegramChannelScraper.py
+++ TelegramChannelScraper.py
@@ -1,166 +1,203 @@
-# Need to pip / conda install
-from telethon import TelegramClient # v1.25.2
+# Pip / Conda install
+from telethon import TelegramClient
 import pandas as pd
-# Included with Python
+# Python standard libraries
 from datetime import datetime as dt
 from time import sleep
 import os
 
 class TelegramChannelScraper():
-    def __init__(self):
-        self.credentials = {}
-        credentials_provided = False
-        
-        while not credentials_provided:
-            self.credentials["api_id"] = input(
-                "\nProvide your Telegram api_id:\n(Shift + insert to paste)\n").strip()
-            self.credentials["api_hash"] = input(
-                "\nProvide your Telegram api_hash:\n(Shift + insert to paste)\n").strip()
-            self.credentials["phone"] = input(
-                "\nProvide your Telegram account phone number:\n(Shift + insert to paste)\n").strip()
-            print(f"\nThese are the credentials you provided:\n{self.credentials}\n")
-            user_accepts = input(
-                "\nDo you wish to proceed? Enter 'y' to continue or 'n' to start over:\n")
-            
-            if user_accepts.strip().lower() == 'y':
-                credentials_provided = True
-        
-    async def start_client(self):
+    def __init__(self, credentials={}):
+        self.client = None
+        self.is_signed_in = False
+        self.credentials = credentials
+
+        self.__is_credentials()
+
+    def __is_credentials(self):
+        if not self.credentials:
+            raise Exception(
+                "You must provide your Telegram API credentials! " +
+                "(API ID, API hash, and account phone number)")
+    
+    # ////////// Client initialization, connection, and authorization //////////
+    async def __init_connect_authorize_client(self):
+        if not self.client:
+            self.__init_client()
+            print("Client initialized")
+        else:
+            pass
+        if not self.client.is_connected():
+            await self.client.connect()
+            print("Client connected")
+        else:
+            pass
+        if not await self.client.is_user_authorized():
+            await self.__authorize_client()
+            print("Client authorized")
+        else:
+            pass
+        self.is_signed_in = True
+
+    def __init_client(self):
+        print("Initializing Telegram client...")
         self.client = TelegramClient(
             "", 
             self.credentials["api_id"], 
             self.credentials["api_hash"],
         )
-        print("\n------------------------------\n")
-        print("\nClient initialized\n")
-        await self.authenticate()
+        
+    async def __authorize_client(self):
+        print("Not authenticated, signing in...")
+        await self.client.send_code_request(self.credentials["phone"])
+        await self.client.sign_in(
+            self.credentials["phone"], 
+            input("Enter one-time login code sent to account phone number:")
+        )
 
-    async def authenticate(self):        
-        await self.client.connect()
-        print("Client connected\n")
-
-        is_auth = await self.client.is_user_authorized()
-        if not is_auth:
-            print("Not authenticated; signing in\n")
-            await self.client.send_code_request(self.credentials["phone"])
-            await self.client.sign_in(
-                self.credentials["phone"], 
-                input("Enter code:\n")
+    # ////////// Get messages and helpers //////////
+    async def get_messages(self, channel_name, start_message_id=None):
+        if not self.is_signed_in:
+            await self.__init_connect_authorize_client()
+        await self.__verify_channel_name_and_exists(channel_name)
+        output_folder_name = self.__create_output_folder(channel_name)
+        await self.__pull_messages_in_batches(
+            channel_name, 
+            start_message_id, 
+            output_folder_name,
             )
-            print("\nClient authenticated\n\n")
-        
-    async def get_messages(self):
-        self.today_date = dt.today().strftime("%Y-%m-%d")
-        self.target_channel = None
-        
-        channel_provided = False
-        while not channel_provided:
-            print("\n------------------------------\n")
-            entered_channel = input("Enter a Telegram channel to scrape:\n")
-            if entered_channel.strip() == "":
-                print("You must enter a valid channel name!\n")
-                continue
-
-            print(f"\nThis is the channel you provided:\n{entered_channel}\n")
-            user_accepts = input(
-                "Do you wish to proceed? Enter 'y' to continue or 'n' to start over:\n")
-            
-            if user_accepts.strip().lower() == 'y':
-                self.target_channel = entered_channel
-                channel_provided = True
-                
-        self.folder_name = f"Output_{self.target_channel}_{self.today_date}"
-        
-        try:
-            os.mkdir(self.folder_name)
-        except FileExistsError:
-            pass
-        print(f"\nCreated folder '{self.folder_name}' to store output\n")
-
-        pulled_messages = await self.pull_messages_in_batches()
-        cleaned_messages = self.parse_raw_messages(pulled_messages)
-        self.create_messages_table(cleaned_messages)
-        print(f"[Finished]")
-
-    async def pull_messages_in_batches(self):
-        # Find highest message ID; this is most recent message
-        all_messages = await self.client.get_messages(self.target_channel)
-        highest_message_id = all_messages[0].id
-        print(f"Most recent message in channel '{self.target_channel}' has ID {highest_message_id}\n")
+        print(f"\nAppended messages to file '{output_folder_name}.csv' in folder '{output_folder_name}'")
+        print(f"Finished pulling messages for channel '{channel_name}'")
     
-        # Divide highest ID by 1000 and floor to get number of batches
-        batches_needed = int(highest_message_id / 1000)
-        print(f"Preparing to pull messages in {batches_needed} batches of 1000 each, oldest to newest\n")
+    async def __verify_channel_name_and_exists(self, channel_name):
+        if not channel_name.strip():
+            raise Exception("You must provide a non-empty channel name!")
+        try:
+            channel_info = await self.client.get_entity(channel_name)
+        except ValueError:
+            raise Exception("No user or channel with that name! (Try another channel name)")
+        return
+    
+    def __create_output_folder(self, channel_name):
+        today_date = dt.today().strftime("%Y-%m-%d_%H%M")
+        output_folder_name = f"TelegramChannelScraper_'{channel_name}'_{today_date}"
+        try:
+            os.mkdir(output_folder_name)
+            print(f"\nCreated folder '{output_folder_name}' to store output")
+        except FileExistsError:
+            print(f"\nFound existing folder {output_folder_name}' to store output")
+            pass
+        return output_folder_name
 
-        # Loop through batches pulling 1000 msgs at a time; final batch must encompass highest message ID
-        pulled_messages = []
-        for i in range(1, batches_needed + 2): # add 2 to be high-inclusive for highest ID
-            batch_high = i * 1000
-            batch_low = batch_high - 999
-            print(f"Working on batch {i} (message ID: {batch_low} - message ID: {batch_high})")
+    async def __pull_messages_in_batches(self, channel_name, start_message_id, output_folder_name):
+        # Find the current highest message ID (most recent message)
+        all_messages = await self.client.get_messages(channel_name)
+        highest_message_id = all_messages[0].id
+        # highest_message_id = 6399 # TESTING ONLY
+        print(f"Current highest message ID is {highest_message_id}")
+        
+        if start_message_id:
+            self.__validate_start_message_id(highest_message_id, start_message_id)
 
+        num_batches_need = self.__determine_num_batches_needed(highest_message_id, start_message_id)
+        print(f"Need {num_batches_need} batches to pull messages for channel")
+    
+        # Slider is used to determine where to start each batch of 1000 and is incremented 
+        # to the next 1000 on each pass until reaching the current highest message ID
+        batch_slider = start_message_id if start_message_id else 0
+
+        for i in range(1, num_batches_need + 1):
+            batch_low_message_id = batch_slider
+            batch_high_message_id = ((batch_low_message_id + 999) 
+                                     if (batch_low_message_id + 999) <= highest_message_id 
+                                     else highest_message_id)
+            batch_slider = batch_high_message_id + 1
+            
+            print(f"\nWorking on batch {i} (message ID: {batch_low_message_id} - " +
+                  f"message ID: {batch_high_message_id})")
+
+            raw_batch_messages = []
             async for message in self.client.iter_messages(
-                self.target_channel, 
+                channel_name, 
                 limit=1000, 
-                min_id=batch_low,
-                max_id=batch_high,
-                reverse=True): # oldest to newest
-
-                pulled_messages.append(message)
-                
+                min_id=batch_low_message_id - 1,# include message _000 instead of starting at _001
+                max_id=batch_high_message_id + 1, # include message _999 instead of stopping at __998
+                reverse=True, # oldest to newest
+                ):
+                raw_batch_messages.append(message)
+            parsed_batch_messages = self.__parse_raw_messages(raw_batch_messages)
+            self.__append_messages_to_file(parsed_batch_messages, output_folder_name)
             sleep(30.0)
-            print(f"Finished batch {i}\n")
+            print(f"Finished batch {i}")
+    
+    def __validate_start_message_id(self, highest_message_id, start_message_id):
+        if start_message_id < 0:
+            raise Exception("Provided start message ID is invalid (less than 0)")
+        if start_message_id > highest_message_id:
+            raise Exception("Provided start message ID is greater than the current highest message ID")
+        return
+    
+    def __determine_num_batches_needed(self, highest_message_id, start_message_id):
+        """
+        Returns the number of batches need to include all messsages. 
+        
+        Example: 
+        Highest message ID is 6399. 6399 / 1000 = 6.399, rounded down to int 6. But add
+        1 so total number of batches needed is 7 as 6 batches would only cover up to 5999.
+        7 batches would cover up to message ID 6999. Batch 0: 0-999, batch 1: 1000-1999, 
+        batch 2: 2000-2999, batch 3: 3000-3999, batch 4: 4000-4999, batch 5: 5000-5999, 
+        batch 6: 6000-6999. (Starting from 0 this is 7 batches.)
 
-        print(f"Pulled {len(pulled_messages)} total messages for channel '{self.target_channel}'\n")
-        return pulled_messages
+        Example:
+        Highest message ID is 6399 and start message ID is 5000. (6399 - 5000) / 1000 is 
+        1.399, rounded down to int 1. But add 1 to make total number of batches needed 2: 
+        batch 0 would cover 5000-5999 and batch 1 would cover 6000-6999. (Starting from 0 
+        this is 2 batches.)
+        """
+        batch_size = 1000
+        if not start_message_id:
+            return (int(highest_message_id / batch_size) + 1)
+        else:
+            return (int((highest_message_id - start_message_id) / batch_size) + 1)
 
-    def parse_raw_messages(self, pulled_messages):
-        print("Extracting fields and cleaning messages\n")
+    def __parse_raw_messages(self, pulled_messages):
+        print("Extracting fields and cleaning messages...")
         cleaned_messages = []
         for message in pulled_messages:
-            cleaned_messages.append(self.extract_message_fields(message))
-        print(f"Cleaned {len(cleaned_messages)} messaged\n")
+            cleaned_messages.append(self.__extract_message_fields(message))
         return cleaned_messages
-    
-    def extract_message_fields(self, message):
-        # Messaage info: https://docs.telethon.dev/en/stable/quick-references/objects-reference.html
-        cleaned = {}
 
+    def __extract_message_fields(self, message):
+        # Message info: https://docs.telethon.dev/en/stable/quick-references/objects-reference.html
+        cleaned = {}
         try:
             cleaned['message_id'] = message.id
         except AttributeError:
             cleaned['message_id'] = None
-
         try:
             cleaned['channel_id'] = message.peer_id.channel_id
         except AttributeError:
             cleaned['channel_id'] = None
-
         try:
             cleaned['message_date'] = str(message.date)
         except AttributeError:
             cleaned['message_date'] = None
-
         try:
             cleaned['post_author'] = message.post_author
         except AttributeError:
             cleaned['post_author'] = None
-
         try:
             cleaned['is_posted_by_bot'] = message.via_bot_id
         except AttributeError:
             cleaned['is_posted_by_bot'] = None
-
         try:
             cleaned['total_replies'] = message.replies.replies
         except AttributeError:
             cleaned['total_replies'] = None
-
         try:
             cleaned['is_reply_comments'] = message.replies.comments
         except AttributeError:
             cleaned['is_reply_comments'] = None
-
         try:
             # Sometimes recent_repliers list is there but it can be empty
             if message.replies.recent_repliers and len(message.replies.recent_repliers) > 0:
@@ -170,21 +207,22 @@ class TelegramChannelScraper():
                 cleaned['recent_replier_ids'] = []
         except AttributeError:
             cleaned['recent_replier_ids'] = []
-
         try:
             cleaned['raw_text'] = message.raw_text
         except AttributeError:
             cleaned['raw_text'] = None
-
         return cleaned
 
-    def create_messages_table(self, cleaned_messages):
-        print("Making messages table\n")
+    def __append_messages_to_file(self, cleaned_messages, output_folder_name):
+        print("Appending messages to file...")
         df = pd.DataFrame(cleaned_messages)
         df = df.explode("recent_replier_ids") # should be 1 ID per row
         df.fillna("", inplace=True)
         df.drop_duplicates(inplace=True)
-        # print(f"Messages table rows: {df.shape[0]}")
-        # Needs a try except to handle if already try to create file and failed (CSV will still be there)
-        df.to_csv(f"{self.folder_name}/{self.folder_name}.csv", index=False)
-        print(f"Created file '{self.folder_name}.csv' in output folder\n\n")
+        full_output_file_path = f"{output_folder_name}/{output_folder_name}.csv"
+        # If file does not yet exist, write file with headers
+        if not os.path.isfile(full_output_file_path):
+            df.to_csv(full_output_file_path, index=False)
+        # Otherwise append new data without headers
+        else:
+            df.to_csv(full_output_file_path, mode="a", header=False, index=False)
